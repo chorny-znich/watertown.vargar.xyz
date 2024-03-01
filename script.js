404f13664d1c2a02ec1c9626de0246a3d041af06
chorny-znich <vargar.dt@gmail.com>
2024-03-01 16:49:19 +0300

    feat: add game creenshots, change css rules for the screenshots section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const SMALL_SCREENSHOT_NUMBER = 4;
+const SMALL_SCREENSHOT_NUMBER = 5;
 const SMALL_SCREENSHOT_SIZE = {
-  width: 250,
-  height: 150,
+  width: 200,
+  height: 120,
 };
 window.addEventListener('load', () => {
   run();
@@ -13,11 +13,11 @@ function run() {
   
   for (let i = 1; i <= SMALL_SCREENSHOT_NUMBER; i++) {
     let img = new Image(SMALL_SCREENSHOT_SIZE.width, SMALL_SCREENSHOT_SIZE.height);
-    img.src = `./assets/img/screenshot_small_${i}.jpg`;
+    img.src = `./assets/img/screenshots/screenshot_small_${i}.jpg`;
     img.alt = "game's screenshot in the small size";
     img.className = 'screenshot-small';
     img.addEventListener('click', () => {
-      bigScreenshot.src = `./assets/img/screenshot_big_${i}.jpg`;
+      bigScreenshot.src = `./assets/img/screenshots/screenshot_big_${i}.jpg`;
     });
     screenshotContainer.appendChild(img);
   }
